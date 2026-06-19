d445203c69edb11c8b206d8fe0c1f757a9c761e5
mohsen-akbari <crtmohsen@gmail.com>
2026-06-19 22:25:50 +0330

    [ADD] more content

--- src/app/doctor/[id]/page.jsx
+++ src/app/doctor/[id]/page.jsx
@@ -1,8 +1,16 @@
+
 import React from "react";
 
 import { notFound } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
+
+import MingcuteLocationLine from "@/icons/MingcuteLocationLine";
+import MingcuteStarFill from "@/icons/MingcuteStarFill";
 
 import { doctors } from "@/mock/doctors";
+
+import styles from "./page.module.css";
 
 export default function Page({ params }) {
   const doctor = doctors.find((doctor) => params.id === doctor.id);
@@ -11,5 +19,45 @@ export default function Page({ params }) {
     return notFound();
   }
 
-  return <div>{doctor?.name}</div>;
+  return (
+    <div className={styles.page}>
+      <div className={styles.header}>
+        <div className={styles.image}>
+          <Image
+            src={`https://cdn.paziresh24.com${doctor.image}`}
+            alt="عکس پروفایل دکتر"
+            width={150}
+            height={150}
+          />
+        </div>
+        <div className={styles.name}>{doctor.name}</div>
+        <div className={styles.brief}>{doctor.brief}</div>
+        <div className={styles.badges}>
+          {doctor.badges.map((badge) => (
+            <div key={badge} className={styles.badge}>
+              {badge}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className={styles.address}>
+        <MingcuteLocationLine /> آدرس: {doctor.address}
+      </div>
+      <div className={styles.actions}>
+        <div className={styles.rating}>
+          <MingcuteStarFill className={styles.icon} />{" "}
+          <span className={styles["average-rating"]}>
+            {Math.floor(doctor.averageRating * 10) / 10}
+          </span>{" "}
+          <span className={styles["total-votes"]}>
+            ({doctor.totalVotes} نظر)
+          </span>
+        </div>
+        <div className={styles.caption}>
+          اولین نوبت: {doctor.firstAvailableAppointment}
+        </div>
+        <Link href={`/doctor/${doctor.id}`}>نوبت‌دهی آنلاین</Link>
+      </div>
+    </div>
+  );
 }
