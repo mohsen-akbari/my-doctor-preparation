f681efd1930d5ffc3148b6dab4fa7b19365a92bb
mohsen-akbari <crtmohsen@gmail.com>
2026-06-23 23:53:35 +0330

    [ADD] username and password validation

--- src/app/api/auth/sign-up/route.js
+++ src/app/api/auth/sign-up/route.js
@@ -12,6 +12,23 @@ export async function POST(request) {
       return NextResponse.json({ error: parsError }, { status: 400 });
     }
 
+    let foundUser = await prisma.user.findUnique({
+      where: { username: body.username },
+    });
+
+    if (foundUser) {
+      return NextResponse.json(
+        { error: "نام کاربری تکراری است." },
+        { status: 400 },
+      );
+    }
+
+    foundUser = await prisma.user.findUnique({ where: { email: body.email } });
+
+    if (foundUser) {
+      return NextResponse.json({ error: "ایمیل تکراری است." }, { status: 400 });
+    }
+
     await prisma.user.create({ data: body });
 
     return NextResponse.json({ data: null }, { status: 201 });
